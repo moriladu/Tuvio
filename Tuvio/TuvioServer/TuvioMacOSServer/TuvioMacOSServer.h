baf4732e14763f9621401fa55be367ca7cdb87d9
20eml5 <20eml5@tcl217a-18.cs.williams.edu>
2019-01-04 18:16:44 -0500

    added a new target for macOS

---
+++ Tuvio/TuvioServer/TuvioMacOSServer/TuvioMacOSServer.h
@@ -0,0 +1,19 @@
+//
+//  TuvioMacOSServer.h
+//  TuvioMacOSServer
+//
+//  Created by 20eml5 on 1/4/19.
+//  Copyright © 2019 20eml5. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TuvioMacOSServer.
+FOUNDATION_EXPORT double TuvioMacOSServerVersionNumber;
+
+//! Project version string for TuvioMacOSServer.
+FOUNDATION_EXPORT const unsigned char TuvioMacOSServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TuvioMacOSServer/PublicHeader.h>
+
+
