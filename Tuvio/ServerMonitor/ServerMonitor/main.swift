45b3f8fa2cf5e1fd9c5baad53f190d62e24054e5
moriladu <30448770+moriladu@users.noreply.github.com>
2019-01-04 18:19:17 -0500

    Add files via upload

---
+++ Tuvio/ServerMonitor/ServerMonitor/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  ServerMonitor
+//
+//  Created by 20eml5 on 1/4/19.
+//  Copyright © 2019 20eml5. All rights reserved.
+//
+
+import Foundation
+import TuvioMacOSServer
+
+print("Hello, World!")
+
